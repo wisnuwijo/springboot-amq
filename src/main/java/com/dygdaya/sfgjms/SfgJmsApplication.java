6037bc2aaaf4ac8ef5e65a013e114f6ac4b422ed
wisnuwijo <wisnuwijo33@gmail.com>
2022-05-08 12:49:25 +0700

    remove activemq server

--- src/main/java/com/dygdaya/sfgjms/SfgJmsApplication.java
+++ src/main/java/com/dygdaya/sfgjms/SfgJmsApplication.java
@@ -10,16 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgJmsApplication {
 
 	public static void main(String[] args) throws Exception {
-
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-				.setPersistenceEnabled(false)
-				.setJournalDirectory("target/data/journal")
-				.setSecurityEnabled(false)
-				.addAcceptorConfiguration("invm","vm://0")
-		);
-
-		server.start();
-
 		SpringApplication.run(SfgJmsApplication.class, args);
 	}
 
